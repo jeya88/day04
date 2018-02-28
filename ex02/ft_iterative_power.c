b0f11d4ce1f4e169bc94a46822f463eb1445ec93
Jeyatharsan SRILINGAM <jsriling@e3r9p5.42.fr>
2018-02-28 20:38:01 +0100

    day4

---
+++ ex02/ft_iterative_power.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsriling <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/16 03:19:09 by jsriling          #+#    #+#             */
+/*   Updated: 2018/02/22 15:34:25 by jsriling         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int result;
+
+	result = 1;
+	if (power < 0)
+		return (0);
+	while (power > 0)
+	{
+		result = result * nb;
+		power--;
+	}
+	return (result);
+}
